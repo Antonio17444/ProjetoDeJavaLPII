e0a5a1d145eea25da3614b9085f98c9678df6426
Antonio Henrique <antoniohenrique1305l@gmail.com>
2026-03-30 11:19:04 -0300

    .

--- src/projeto/jogos/Cyberpunk.java
+++
@@ -1,8 +0,0 @@
-package projeto.jogos;
-
-public class Cyberpunk extends Jogo{
-	public Cyberpunk(){
-		super("Cyberpunk", "Open-Word", 199.0f, 70.0f, 2026, "CD Projekt RED", 0);
-	}
-
-}
--- src/projeto/jogos/StardewValley.java
+++
@@ -1,9 +0,0 @@
-package projeto.jogos;
-
-public class StardewValley extends Jogo {
-	public StardewValley(){
-		super("Stardew Valley", "Simulação", 25.0f, 1.0f, 2016, "ConcernedApe", 9);
-	}
-
-	
-}
